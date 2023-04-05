e871be2b0e49fa254fb0ca796d78836487709247
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-04-05 18:58:25 +0100

    task 101 done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+void printInt(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+
+	int divisor = 1;
+
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{
+		putchar(n / divisor + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}
+
+int main(void)
+{
+	int n = 12345;
+
+	printInt(n);
+	return (0);
+}
+
+/*#include <stdio.h>
+void printInt(int n) {
+    if(n < 0) {
+        putchar('-');
+        n = -n;
+    }
+    do {
+        putchar(n%10 + '0');
+        n /= 10;
+    } while(n);
+}
+int main() {
+    int n = 12345;
+    printInt(n);
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+void printInt(int n) {
+    if(n < 0) {
+        putchar('-');
+        n = -n;
+    }
+    if(n/10)
+        printInt(n/10);
+    putchar(n%10 + '0');
+}
+int main() {
+    int n = 12345;
+    printInt(n);
+    return 0;
+}
+
+The program first takes an input number from the user.
+It then checks if the number is negative.
+If the number is negative, it prints a ‘-’ sign and makes the number positive.
+It then checks if the number has more than one digit.
+If it does, it calls itself recursively with the quotient of the number divided by 10.
+Finally, it prints the last digit of the number.
+*/
+
