4eb3b06a58ec7c2fcaea556eb1670c9b55c29bc1
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-04-19 09:38:04 +0100

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
