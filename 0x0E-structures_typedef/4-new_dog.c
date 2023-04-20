910a3fab1112457a1e0d1af6ef7a509ea0c6c802
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-04-20 05:06:07 +0100

    task4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+
+/**
+ * new_dog - function
+ * @name: the name of the dog
+ * @age: holds the age integer
+ * @owner: holds the owner
+ * Return: the pointer to the dog
+ * Description: store a copy of name and owner
+ *		Return NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
